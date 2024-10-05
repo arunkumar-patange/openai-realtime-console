ef23f818c8a45ab2bfb8b2e074134a167151dd91
apatange <apatange@brain.im>
2024-10-04 23:14:52 -0700

    update

--- src/components/RestaurantModal.tsx
+++ src/components/RestaurantModal.tsx
@@ -25,32 +25,40 @@ const RestaurantModal: React.FC<RestaurantModalProps> = ({ restaurants, generate
     <div className="restaurant-modal">
       <div className="modal-content">
         <span className="close" onClick={onClose}>&times;</span>
-        <h2>Restaurants</h2>
-        <ul>
-          {restaurants.map((restaurant) => (
-            <li key={restaurant.id}>
-              <h3>{restaurant.name}</h3>
-              <img src={restaurant.image_url} alt={restaurant.name} />
-              <p>{restaurant.location.address1}, {restaurant.location.city}</p>
-            </li>
-          ))}
-        </ul>
-        {generatedImage && (
+        {restaurants.length > 0 && (
+          <>
+            <h2>Restaurants</h2>
+            <ul>
+              {restaurants.map((restaurant) => (
+                <li key={restaurant.id}>
+                  <h3>{restaurant.name}</h3>
+                  <img src={restaurant.image_url} alt={restaurant.name} />
+                  <p>{restaurant.location.address1}, {restaurant.location.city}</p>
+                </li>
+              ))}
+            </ul>
+          </>
+        )}
+        {generatedImage && restaurants.length === 0 && searchResults.length === 0 && (
           <div className="generated-image">
             <h3>Generated Image</h3>
             <img src={generatedImage} alt="Generated" />
           </div>
         )}
-        <h2>Search Results</h2>
-        <ul>
-          {searchResults.map((result, index) => (
-            <li key={index}>
-              <h3>{result.title}</h3> {/* Title of the search result */}
-              <a href={result.url} target="_blank" rel="noopener noreferrer">{result.url}</a> {/* Link to the result */}
-              <p>{result.content}</p> {/* Content snippet of the result */}
-            </li>
-          ))}
-        </ul>
+        {searchResults.length > 0 && restaurants.length === 0 && generatedImage === null && (
+          <>
+            <h2>Search Results</h2>
+            <ul>
+              {searchResults.map((result, index) => (
+                <li key={index}>
+                  <h3>{result.title}</h3> {/* Title of the search result */}
+                  <a href={result.url} target="_blank" rel="noopener noreferrer">{result.url}</a> {/* Link to the result */}
+                  <p>{result.content}</p> {/* Content snippet of the result */}
+                </li>
+              ))}
+            </ul>
+          </>
+        )}
       </div>
     </div>
   );
